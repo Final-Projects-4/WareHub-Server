061d8a18d1be0c89b2544de434b49348af6b3082
Yulius Pratama <yuliuspratama9e@gmail.com>
2023-04-14 17:17:47 +0700

    Add Routers Customer

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const routerCategory = require("./routerCategory");
+const routerCustomer = require("./routerCustomer");
 
 router.use(routerCategory);
+router.use(routerCustomer);
 
 module.exports = router;
