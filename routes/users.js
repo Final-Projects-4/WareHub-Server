7086cbc12870922508150ba897187076cbc1ccff
Vincent Kho <vincentkho67@gmail.com>
2023-04-14 16:13:39 +0700

    Register & Login method added.

--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,22 @@ const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/userController');
 
-router.get('/', UserController.getAllUsers);
-router.get('/:id', UserController.getUserById);
-router.post('/', UserController.createUser);
-router.put('/:id', UserController.updateUser);
-router.delete('/:id', UserController.deleteUser);
+
+
+// router.get('/', UserController.getAllUsers);
+// router.get('/:id', UserController.getUserById);
+router.post('/register', async (req, res) => {
+    const { first_name, last_name, email, username, password, address, company } = req.body;
+  
+    try {
+      const user = await UserController.register(first_name, last_name, email, username, password, address, company);
+      res.status(201).json({ message: 'User created successfully', user });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: 'Error creating user' });
+    }
+  });
+// router.put('/:id', UserController.updateUser);
+// router.delete('/:id', UserController.deleteUser);
 
 module.exports = router;
