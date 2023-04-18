a819ebbba9d1c6d991b3acb850428ce0f9c5e57a
Vincent Kho <vincentkho67@gmail.com>
2023-04-18 08:06:41 +0700

    minor bug fixed

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,6 @@
-// routes/users.js
 const express = require('express');
 const router = express.Router();
-const UserController = require('../controllers/UserController');
+const UserController = require('../controllers/userController');
 
 router.post('/register', UserController.register);
 router.post('/login', UserController.login);
