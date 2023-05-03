890142672d47f4d8940811521318fd3369d4f63d
Vincent Kho <vincentkho67@gmail.com>
2023-05-03 21:29:36 +0700

    addstock Feature added, user binded product,warehouse & vendor only

--- routes/product.js
+++ routes/product.js
@@ -6,6 +6,7 @@ const ProductController = require('../controllers/productController');
 router.get('/', ProductController.getAll);
 router.get('/:id', ProductController.getOne);
 router.post('/create', ProductController.create);
+router.post('/stocks', ProductController.addStock);
 router.put('/:id', ProductController.update);
 router.delete('/:id', ProductController.delete);
 
