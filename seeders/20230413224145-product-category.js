f0ff8d57c29b4572318c0faaf4a94b124d8efcc9
vincentkho67 <122125309+vincentkho67@users.noreply.github.com>
2023-05-27 21:20:03 +0700

    change seeders

--- seeders/20230413224145-product-category.js
+++ seeders/20230413224145-product-category.js
@@ -30,6 +30,55 @@ module.exports = {
           createdAt: new Date(),
           updatedAt: new Date(),
         },
+        {
+          product_id: 5,
+          category_id: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          product_id: 6,
+          category_id: 2,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          product_id: 7,
+          category_id: 3,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          product_id: 8,
+          category_id: 4,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          product_id: 9,
+          category_id: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          product_id: 10,
+          category_id: 2,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          product_id: 11,
+          category_id: 3,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          product_id: 12,
+          category_id: 4,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        
       ],
       {}
     );
